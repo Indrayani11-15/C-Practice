c24f4d78d95b41f9bbbf881292629a46aff72e4f
Indrayani11-15 <verulkarindrayani@gmail.com>
2024-12-22 09:51:30 +0530

    basic variable and constants

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int score;
+    score = 110;
+
+    const int uid = 232323; //cant be changed in future
+    int balance = 500;
+
+    cout<<"Welcome to chai with cpp 1"<<endl;
+    cout<<"Welcome to chai with cpp 2"<<endl;
+    cout<<"Welcome to chai with cpp 3"<<endl;
+    return 0;
+}
