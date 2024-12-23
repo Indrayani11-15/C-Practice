6f9474f856edd21ca1f4d22236267d36bb714b17
Indrayani11-15 <verulkarindrayani@gmail.com>
2024-12-23 19:55:10 +0530

    que

---
+++ 02_primitives/challenge1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string teaType = "Lemon Tea";
+    float teaPrice= 45.99;
+    char teaRating = 'A';
+
+    cout<<teaType<<endl;
+    cout<<teaPrice<<endl;
+    cout<<teaRating<<endl;
+
+
+    return 0;
+}
