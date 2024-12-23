9120143b8aff4dad9b699d993442ece2d99b650a
Indrayani11-15 <verulkarindrayani@gmail.com>
2024-12-23 10:53:59 +0530

    userinput

--- 02_primitives/casting.cpp
+++ 02_primitives/casting.cpp
@@ -5,6 +5,9 @@ int main(){
     float teaPrice = 49.99;
     int roundedTeaPrice = (int) teaPrice;
 
-    cout<<roundedTeaPrice<<endl;
+    int teaQuality = 2;
+    double totalPrice = teaPrice * teaQuality;
+
+    cout<<totalPrice<<endl;
     return 0;
 }
