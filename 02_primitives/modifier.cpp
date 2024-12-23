66821b9ecaca0cf7d95b45ce63bdbf6fdcb03dfd
Indrayani11-15 <verulkarindrayani@gmail.com>
2024-12-23 10:24:58 +0530

    data types and casting

---
+++ 02_primitives/modifier.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    unsigned smallTeaPack = 100;
+
+    long largeTeaStorage = 100000;
+    cout<<largeTeaStorage<<endl;
+
+
+
+
+
+
+
+    return 0;
+}
