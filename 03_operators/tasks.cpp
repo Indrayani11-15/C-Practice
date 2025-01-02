4b73f6bcc54d5d77fb9aabff5e207500efffdf4a
Indrayani11-15 <verulkarindrayani@gmail.com>
2025-01-02 10:55:48 +0530

    challenges

---
+++ 03_operators/tasks.cpp
@@ -0,0 +1,18 @@
+// Arithmetic Operators
+// 1) Create a program that calculates the total price of tea cups. the user input the number of cups they want and the price per cup. the program should contain the total price, apply a 5% discount if the total is above a certain amount and show the final price.
+
+
+//Assignment Operators
+//Write a program that allows a use to input the number of tea bags they have. Assign additional bags to them based on certain conditions(e.g they have fewer than 10 bags, give them 5 extra).Update the original number using assignment operator.
+
+
+//Relational OPerator
+//A tea shop offers a loyalty program.Customers who buy more than 20 cups of tea get a special "Gold" badge, and those who buy 10 to 20 cups get a "Silver" badge.Write a program to display the badge they will receive on the number of cups they buy.
+
+
+//Logical Operators
+//Create a program that check if a user is eligible for a tea subcription discount. The discount applies if the user is either a studemt or has purchased more than 15 cups.Ask the user to input their status(student or not) and their cup count.
+
+//Bitwise Operators
+
+
