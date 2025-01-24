b59ae8ca3b2b5defac520f56194d993da4a8a18d
Indrayani Verulkar <118429007+Indrayani11-15@users.noreply.github.com>
2025-01-24 19:58:16 +0530

    Loops

---
+++ 05_loops/challenges.cpp
@@ -0,0 +1,40 @@
+//While Loop
+//Write a program that keeps track of tea orders. Each time a cup of teais made decrease the number of cups remaining.The loop should run until all cups are served.
+ #include<iostream>
+ #include<string>
+ using namespace std;
+ int main(){
+    int teaCups;
+    cout<<"enter the number of tea cups to serve: ";
+    cin>>teaCups;
+
+    while (teaCups>0)
+    {
+        cout<<"Serving cup of tea \n"<<teaCups<<"remaining"<<endl;
+        teaCups--;
+    }
+    cout<<"All tea cups are served"<<endl;
+    
+
+ }
+
+
+//Do-While loop
+// Create a program that asks if they want more tea.Keep asking them until they type"no"(case-sensitive), using a do-while loop.
+#include<iostream>
+ #include<string>
+ using namespace std;
+ int main(){
+    
+
+
+    return 0;
+ }
+
+
+
+//For Loop
+//Write a program that prints the breing instructions for making 5 cups of tea.The brewing process should be printed once for each cup using a for loop.
+
+
+ 
