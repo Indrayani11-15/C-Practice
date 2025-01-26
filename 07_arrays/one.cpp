a909aef36ca83a90ddf24b7c6a71e75b58f9774f
Indrayani Verulkar <118429007+Indrayani11-15@users.noreply.github.com>
2025-01-26 11:19:49 +0530

    array's basic

---
+++ 07_arrays/one.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+ int main(){
+    int chaiTemperature[5] = {85,90,88,92,98};
+    cout<<"Chai temperatures: ";
+    for(int i = 0; i < 5; i++){
+        cout<<chaiTemperature[i]<<" degree C \n";
+    }
+
+
+    return 0;
+ }
---
+++ 07_arrays/two.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+ int main(){
+    int chaiServe[7]= {50,34,33,21,98,60,54};
+    cout<<"Chai cups served on day 1: "<<chaiServe[0];
+    cout<<"Chai cups served on day 2: "<<chaiServe[1];
+    cout<<"Chai cups served on day 3: "<<chaiServe[2];
+    cout<<"Chai cups served on day 4: "<<chaiServe[3];
+    return 0;
+ }
